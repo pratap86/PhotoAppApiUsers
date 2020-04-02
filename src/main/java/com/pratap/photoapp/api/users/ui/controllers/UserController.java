a34ec193bd35c549719a436028e57777c7903b79
pratap86 <narayanpratap86@gmail.com>
2020-04-02 22:44:06 +0530

    spring cloud bus implemented

--- src/main/java/com/pratap/photoapp/api/users/ui/controllers/UserController.java
+++ src/main/java/com/pratap/photoapp/api/users/ui/controllers/UserController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -27,10 +28,13 @@ public class UserController {
 
 	@Autowired
 	ModelMapper modelMapper;
+	
+	@Autowired
+	Environment env;
 
 	@GetMapping("/status/check")
 	public String getStatus() {
-		return "working";
+		return "working on port :"+env.getProperty("local.server.port")+" , with token = "+env.getProperty("token.secret");
 	}
 
 	@PostMapping(
